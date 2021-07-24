840320b7befbc28f13d078eb7f83d76e27b19028
rithin <rithin316@gmail.com>
2021-07-24 23:54:59 +0530

    create post -40%

---
+++ static/index.js
@@ -0,0 +1,54 @@
+
+const image_uploadBtn = document.getElementById('image-upload')
+const imgInput = document.getElementById('img-uploadInput');
+const uploadImg = document.getElementById('uploadedImg');
+imgInput.addEventListener('change',function(){
+    if(this.files && this.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e){
+            uploadImg.setAttribute('src',e.target.result);
+            image_uploadBtn.parentElement.setAttribute('style','height:auto;')
+            image_uploadBtn.setAttribute('style','opacity:.9;')
+            uploadImg.setAttribute('style','display:inline;max-height:500px;')
+        }
+        reader.readAsDataURL(this.files[0]);
+
+    }
+})
+image_uploadBtn.addEventListener('click', function(){
+  imgInput.click();
+})
+
+
+function editableMaxLengthValidator(element, max_length) {
+    const validator = (event)=>{
+        if(element.innerText.length > max_length) {
+            if(event.which==8){
+                element.innerText= element.innerText.substring(0,max_length)
+                const selection = window.getSelection();
+                const range = document.createRange();
+                selection.removeAllRanges();
+                range.selectNodeContents(element);
+                range.collapse(false);
+                selection.addRange(range);
+                element.focus();
+                
+            }
+            event.preventDefault();
+        }
+    }
+    element.addEventListener('keydown', function(event){
+        validator(event);
+        
+    })
+}
+
+postTitle = document.getElementById('create-post-title');
+editableMaxLengthValidator(postTitle, 40)
+
+postTitle.addEventListener('keydown', function(event){
+    if(event.which==13){
+        event.preventDefault();
+  
+    }
+})
