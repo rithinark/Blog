91461a609a5b45dfaf5005513630504d69e2d1fe
rithin <rithin316@gmail.com>
2021-07-24 12:57:16 +0530

    created models and attributes

--- app/models.py
+++ app/models.py
@@ -2,3 +2,54 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class BlogUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_img = models.ImageField(upload_to='images/profiles')
+
+
+class Tag(models.Model):
+    tags = models.CharField(max_length=16)
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    title = models.CharField(max_length=32)
+    content = models.TextField()
+    published_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    tumbnail = models.ImageField(upload_to='images/posts')
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.CharField(max_length=4000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Vote(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    vote = models.IntegerField(
+        choices = (
+            ('UPVOTE', 'upvote'),
+            ('DOWNVOTE', 'downvote')
+        )
+    )
+
+
+class Follower(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
+    followed_on = models.DateTimeField()
+
+
+class BookMark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
+
+
+
+
+
