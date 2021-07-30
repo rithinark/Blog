8ff671588035550db2ef22801941f1cbf7f37636
rithin <rithin316@gmail.com>
2021-07-30 22:32:14 +0530

    deplay

--- blog/settings.py
+++ blog/settings.py
@@ -131,8 +131,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
